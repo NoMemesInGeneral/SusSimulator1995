eec3b03d456b60cf7f8ab197f43b0a6ea3736427
Galexion <43537784+TailsEraYT@users.noreply.github.com>
2021-05-31 23:58:28 -0400

    Making More and More Changes.

--- bot.js
+++ bot.js
@@ -2,6 +2,9 @@
 const Discord = require('discord.js');
 const config = require('./const.json');
 const client = new Discord.Client();
+const fetch = require("node-fetch");
+require('discord-buttons')(client);
+const { MessageButton, MessageActionRow } = require('discord-buttons');
 // Link Embed Objects to file
 const embeds = {
     // Fun Commands
@@ -24,14 +27,19 @@ const embeds = {
     }
 };
 client.once('ready', () => {
-    console.log('Ready!');
+    console.log('All 40k of Ram is Smooth!');
     client.user.setActivity('Myself Being rewritten by nondumasses', { type: 'WATCHING' }).catch(console.error);
 });
 
 const prefix = config.prefix;
 
-client.on("message", message => {
+client.on("message", async message => {
         // Only Respond to messages with the prefix
+
+    if (message.content === "20goto10") {
+        return message.channel.send("https://media.giphy.com/media/XbIoQQuFfFIirDn4A0/giphy.gif").catch(console.error);
+    }
+
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
@@ -60,6 +68,26 @@ client.on("message", message => {
         return message.channel.send({ embed: embeds.fun.amongus.embed }).catch(console.error);
     }
 
+  //IF THE FIRST WORD IS gif THEN THE BOT HAS TO FETCH GIF RESULTS BASED ON THE NEXT WORDS
+  if (command === "gif") {
+      console.log(args.length)
+      console.log(args)
+    //WE WILL COMBINE THE WORDS WHICH HAS BEEN SPLITTED EXPECT THE FIRST WORD
+    const keywords = args.slice(1, args.length).join(" ");
+    console.log(keywords)
+    //NOW THIS IS THE API ENDPOINT FROM WHICH WE WILL RECEIVE THE GIFS
+    //HERE WE WILL GET 10 GIFS FROM THIS YOU CAN CHANGE IT YOU WANT
+    const url = `https://api.tenor.com/v1/search?q=${keywords}&key=${config.tenor}&limit=10`;
+    console.log(url)
+    //FETCH THE RESULTS
+    const response = await fetch(url);
+    //CONVERT TO JSON
+    const result = await response.json();
+    //NOW WE CAN RANDOMLY SELECT THE GIF FROM THE RESULTS WE FETCHED
+    const index = Math.floor(Math.random() * result.results.length);
+    //NOW SEND THE RESULT BACK TO SERVER
+    message.channel.send(result.results[index].url);
+  }
 
     // Help and Bot Version commands
 
@@ -68,8 +96,29 @@ client.on("message", message => {
     }
 
     if(command === "help") {
+        // Add Button Funtions
+        
+        let btn1 = new MessageButton().setLabel("Fun").setStyle("blurple").setID("fun-help");
+        let btn2 = new MessageButton().setLabel("Infomation").setStyle("blurple").setID("info-help");
+        let btn3 = new MessageButton().setLabel("Credits").setStyle("blurple").setID("credits");
+        let buttonRow = new MessageActionRow().addComponent(btn1).addComponent(btn2).addComponent(btn3)
+
         //IF no Argument is made, Show the help home menu. (also this also will get messy)
-        if (args.length < 1) return message.channel.send({ embed: embeds.help.home.embed }).catch(console.error);
+        if (args.length < 1) return message.channel.send({component: buttonRow, embed: embeds.help.home.embed },).catch(console.error);
+    }
+});
+
+//Click Button Handler
+client.on('clickButton', async (button) => {
+    if(button.id === 'fun-help') {
+        button.message.channel.send("test");
+    }
+    if(button.id === 'info-help') {
+        button.message.channel.send("test");
+    }
+    if(button.id === 'credits') {
+        button.defer();
+        return button.message.channel.send({ embed: embeds.version.embed });
     }
 });
 
--- events/help.js
+++ events/help.js
@@ -8,7 +8,7 @@ const embed = {
     },
     "author": {
       "name": "SusSimulator1995",
-      "url": "https://discordapp.com",
+        "url": "https://discord.com/api/oauth2/authorize?client_id=848770289239064618&permissions=8&scope=bot",
         "icon_url": "https://cdn.discordapp.com/attachments/824709342447140866/849085995021369454/ventbot1997.png"
     },
     "fields": [
@@ -26,6 +26,11 @@ const embed = {
         "name": "Credits!",
         "value": "Check who made the thing in the first place! (-version)",
         "inline": true
+      },
+      {
+      "name": "10 Print 'Placeholder!'",
+              "value": "20 Goto 10",
+      "inline": true
       }
     ]
 }
